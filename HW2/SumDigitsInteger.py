ca3098a91a3bc1261c452fc6cd7ac68fef070865
RocioPuente <46799522+RocioPuente@users.noreply.github.com>
2019-01-18 15:13:44 -0800

    HW2

--- HW2/SumDigitsInteger.py
+++ HW2/SumDigitsInteger.py
@@ -1,3 +1,13 @@
-print (932 % 10)
-print (93 % 10)
-print (93//10)
+num = int(input("Input a number between 0 and 1000: "))
+
+x  = num //1000
+x1 = (num%10)
+x2 = (num//10)%10
+x3 = (num//100)%100
+print("The sum of digits in the number is", x+x1+x2+x3)
+
+#temp = num
+#resultado = 0
+#for _ in range (3):
+    #resultado = resultado + temp %10
+#print (resultado)
